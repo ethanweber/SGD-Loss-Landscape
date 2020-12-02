ba921483f1cf97e9d7c68b8b5405db9407876d35
Ethan Weber <eweb0124@gmail.com>
2020-12-02 15:37:10 -0500

    progress on data

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,9 +1,11 @@
 import argparse
 import pprint
+from sklearn.datasets import make_regression
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument('--n', type=int, default=1000, help="number of data points")
 parser.add_argument('--d', type=int, default=100, help="feature dimension")
+parser.add_argument('--dataset_name', type=int, default="example_dataset", help="dataset name")
 
 
 if __name__ == "__main__":
@@ -12,5 +14,6 @@ if __name__ == "__main__":
     print("Gengerating dataset with args:")
     pprint.pprint(args)
 
+    dataset = make_regression(n_samples=args.n, n_features=args.d)
+    X, y = dataset
     
-
